3e86f4fe1d435017af3f5ad1fd8d6b1c2332e707
myjeong19 <myjeong19@naver.com>
2025-09-09 20:36:06 +0900

    refactor. 리다이렉트 유틸(api→lib) 이동

--- src/features/auth/login/ui/kakao-loign-button.tsx
+++ src/features/auth/login/ui/kakao-loign-button.tsx
@@ -1,11 +1,11 @@
-import { redirectToKakaoAuth } from "~/features/auth/login/api/redirect-to-kakao-auth";
+import { redirectToKakaoAuth } from "~features/auth/login/lib/redirect-to-kakao-auth";
 
 export function KakaoLoginButton() {
   return (
     <button
       type="button"
       onClick={redirectToKakaoAuth}
-      className="fixed top-10 right-10 z-10 bg-white text-transparent p-3 rounded-md cursor-pointer "
+      className="fixed top-10 right-10 z-10 bg-white text-transparent rounded-md cursor-pointer "
     >
       <img src="/ko-kakao.webp" alt="kakao login button" />
     </button>
